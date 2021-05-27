3373abeddd8a14f533175e47401a650f13407bb4
mdegroot09 <mdegroot09@gmail.com>
2021-05-27 16:43:57 -0600

    learning foreach loop

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new[] {1.1, 2.22, 3.3};
+            List<double> grades = new List<double>();
+            grades.Add(56.1);
+            grades[0]
+
+            var sum = 0.0;
+            foreach(double num in nums)
+            {
+                sum += num;
+            }
+            Console.WriteLine(sum);
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else 
+            {
+                Console.WriteLine("Hello!");
+            }
+        }
+    }
+}

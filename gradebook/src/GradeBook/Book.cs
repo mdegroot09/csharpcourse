6a936b5b66160f40b7fb3b45b60352501ae0ea3e
mdegroot09 <mdegroot09@gmail.com>
2021-06-02 16:21:01 -0600

    added encapsulation in Book class

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -13,6 +14,23 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var lowGrade = double.MaxValue;
+            var highGrade = double.MinValue;
+            foreach(double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            var avg = result / grades.Count;
+            Console.WriteLine($"The lowest grade is {lowGrade}");
+            Console.WriteLine($"The highest grade is {highGrade}");
+            Console.WriteLine($"The average grade is {avg:N1}");
         }
 
         private List<double> grades;
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -11,21 +11,7 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            
-            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
-            grades.Add(56.1);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(double number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-            }
-            var avg = result / grades.Count;
-            Console.WriteLine($"The average grade is {avg:N1}");
+            book.ShowStatistics();
         }
     }
 }

4758ec4da84a0dc6e03d67a800229575999ace75
mdegroot09 <mdegroot09@gmail.com>
2021-06-10 16:21:56 -0600

    refactored Book.cs

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -113,36 +113,10 @@ namespace GradeBook
         public override Statistics GetStatistics()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.Low = double.MaxValue;
-            result.High = double.MinValue;
-            result.Count = grades.Count;
             
             for (var i = 0; i < grades.Count; i++)
             {
-                result.Low = Math.Min(grades[i], result.Low);
-                result.High = Math.Max(grades[i], result.High);
-                result.Average += grades[i];
-            }
-            result.Average /= grades.Count;
-            
-            switch(result.Average)
-            {
-                case var d when d >= 90:
-                    result.Letter = 'A';
-                    break;
-                case var d when d >= 80:
-                    result.Letter = 'B';
-                    break;
-                case var d when d >= 70:
-                    result.Letter = 'C';
-                    break;
-                case var d when d >= 60:
-                    result.Letter = 'D';
-                    break;
-                default:
-                    result.Letter = 'F';
-                    break;
+                result.Add(grades[i]);
             }
             
             return result;
@@ -150,12 +124,6 @@ namespace GradeBook
 
         private List<double> grades;
 
-        public string Name
-        {
-            get; 
-
-            // any operation that wants to write to a private set property outside of Book.cs will not have access
-            set; 
-        }
+        public const string CATEGORY = "Science";
     }
 }
